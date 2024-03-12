a183d1df13f0b578e74cda93adaff9f2b99f375f
Francesco <sworld.design@gmail.com>
2024-03-12 23:02:43 +0000

    removed unused functions

--- Models/UserMessage.cs
+++ Models/UserMessage.cs
@@ -12,34 +12,4 @@ public class UserMessage{
 
     public UserMessage(){}
 
-
-    public UserMessage fromJson(string json){
-        Guid chat_id;
-        Console.WriteLine(json);
-        JObject message = JObject.Parse(json);
-        if(message["chatId"] is not null || message["chatId"]!.ToString() == "" || message["chatId"]!.ToString() == "null"){
-            chat_id = Guid.NewGuid(); 
-        }else {
-            chat_id = Guid.Parse(message["chatId"]!.ToString());
-        }
-
-        Id = Guid.NewGuid();
-        ChatId = chat_id;
-        ProdId = Guid.Parse(message["prodId"]!.ToString());
-        SenderId = message["from"]!.ToString();
-        Message = message["message"]!.ToString();
-        SentAt = ulong.Parse(message["sentAt"]!.ToString());
-        Delivered = false;
-        return this;
-    }
-
-    public string toAppMessage(string jsonMessage){
-        JObject data = JObject.Parse(jsonMessage);
-        string begin = "{";
-        string end = "}";
-        string json = $""" "chatId" : "{ChatId}", "prodId" : "{ProdId}", "from" : "{SenderId}", "to" : "{data["to"]}", "message" : "{Message}", "sentAt" : "{SentAt}" """;
-        json = begin + json.Substring(1, json.Length-1) + end;
-        return json;
-    }
-
 }
